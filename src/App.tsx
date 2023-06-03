694679dca246d66ca99632a6187a2416b8bc0d84
JLake310 <0310kjy@gmail.com>
2023-06-04 02:10:52 +0900

    [Feat] Add vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,20 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainPage from "@pages/Main";
-import ReactGA from "react-ga";
-
-if (process.env.NODE_ENV !== "production") {
-  const gaTrackingId: string = process.env.REACT_APP_GA_TRACKING_ID!;
-  ReactGA.initialize(gaTrackingId, { debug: true });
-  ReactGA.pageview(window.location.pathname);
-}
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<MainPage />}></Route>
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <>
+      <div className="App">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<MainPage />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </div>
+      <Analytics />
+    </>
   );
 }
 
